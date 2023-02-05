52145cac2edf2b67eb7f62b7177cda49038b8978
Yulia Starchenko <imay@amake.ru>
2023-02-05 14:32:34 +0300

    Refactoring: router and routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import 'normalize.css';
 import './styles/index.scss';
-import {Link, Route, Routes} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {cn} from 'shared/lib/classNames/classNames';
 import {useTheme} from 'app/providers/ThemeProvider';
-import {AboutPage} from 'pages/AboutPage';
-import {MainPage} from 'pages/MainPage';
+import {AppRouter} from 'app/providers/router';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -15,12 +14,7 @@ const App = () => {
             <button onClick={toggleTheme}>Сменить тему</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>...Loading</div>}>
-                <Routes>
-                    <Route path={'/'} element={<MainPage/>}/>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };

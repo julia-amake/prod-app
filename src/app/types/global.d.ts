36137a8962b2f5794e54b3a5ea44082b70a2d57d
Yulia Starchenko <imay@amake.ru>
2023-02-06 21:17:00 +0300

    Added: types for ts

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,3 +1,13 @@
 // чтобы импортировать классы из scss-модульных файлов как свойства
-declare module "*.module.css";
-declare module "*.module.scss";
+declare module '*.module.css';
+declare module '*.module.scss';
+
+declare module '*.svg' {
+    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+    export default SVG;
+}
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';

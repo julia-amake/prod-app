a18b03297fed0c0246f33df982e7635f8880f514
Yulia Starchenko <imay@amake.ru>
2023-02-06 21:16:23 +0300

    Updated: classNames util

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,12 +1,10 @@
 //Record здесь создает тип с ключом string и значением boolean | string
 type Mode = Record<string, boolean | string>
 
-export function cn(cls: string, mods: Mode, additional: string[]): string {
+export function cn(cls: string, mods: Mode = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
-        ...Object.entries(mods)
-            .filter(([className, value]) => Boolean(value))
-            .map(([className, value]) => className)
+        ...additional.filter(Boolean),
+        ...Object.keys(mods).filter(key => mods[key])
     ].join(' ');
 }

0c571154c5fa998c11db2da9959e95284d39fe49
Yulia Starchenko <imay@amake.ru>
2023-02-06 21:21:17 +0300

    Updated: App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import 'normalize.css';
 import './styles/index.scss';
-import {Link} from 'react-router-dom';
 import {cn} from 'shared/lib/classNames/classNames';
 import {useTheme} from 'app/providers/ThemeProvider';
 import {AppRouter} from 'app/providers/router';
+import {Navbar} from 'widgets/Navbar';
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={cn('app', {[theme]: true}, [])}>
-            <button onClick={toggleTheme}>Сменить тему</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar/>
             <AppRouter/>
         </div>
     );

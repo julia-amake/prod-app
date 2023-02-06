3cfc955561735b5b68946a23ca8d6211906bd638
Yulia Starchenko <imay@amake.ru>
2023-02-06 22:04:01 +0300

    Uploaded: App and styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,14 +5,18 @@ import {cn} from 'shared/lib/classNames/classNames';
 import {useTheme} from 'app/providers/ThemeProvider';
 import {AppRouter} from 'app/providers/router';
 import {Navbar} from 'widgets/Navbar';
+import {Sidebar} from 'widgets/Sidebar';
 
 const App = () => {
     const {theme} = useTheme();
 
     return (
         <div className={cn('app', {[theme]: true}, [])}>
-            <Navbar/>
-            <AppRouter/>
+            <Sidebar/>
+            <div className="main">
+                <Navbar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };

78cbca4b1efacc4945f345ed16842dc5318080f6
Yulia Starchenko <imay@amake.ru>
2023-02-07 16:22:17 +0300

    Created: LangSwitcher component

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {cn} from 'shared/lib/classNames/classNames';
+import s from './LangSwitcher.module.scss';
+import {useTranslation} from 'react-i18next';
+import Button from 'shared/ui/Button/Button';
+
+interface LangSwitcherProps {
+    className?: string;
+}
+
+const LangSwitcher: React.FC<LangSwitcherProps> = (props) => {
+    const {
+        className,
+    } = props;
+
+    const {t, i18n} = useTranslation();
+
+    const toggleLang = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    };
+
+    return (
+        <Button className={cn(s.langSwitcher, {}, [className])} onClick={toggleLang}>
+            {t('Перевести')}
+        </Button>
+    );
+};
+
+export default LangSwitcher;

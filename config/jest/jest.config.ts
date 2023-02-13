591d1a1280396364c3d8b81443a1da4f6bf66b93
Yulia Starchenko <imay@amake.ru>
2023-02-13 21:06:15 +0300

    Fixed: jest absolute path error

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -25,6 +25,9 @@ export default {
     moduleDirectories: [
         'node_modules',
     ],
+    modulePaths: [
+        '<rootDir>src',
+    ],
     // An array of file extensions your modules use
     moduleFileExtensions: [
         'js',

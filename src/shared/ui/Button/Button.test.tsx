2d1c996e8589244675def323b422524717f34b5a
Yulia Starchenko <imay@amake.ru>
2023-02-14 13:29:32 +0300

    Added: unit test for Button

---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Button, { ThemeButton } from './Button';
+
+describe('Button component', () => {
+    test('button was rendered', () => {
+        render(<Button>Test</Button>);
+        expect(screen.getByText('Test')).toBeInTheDocument();
+    });
+
+    test('button has .clear', () => {
+        render(<Button theme={ThemeButton.CLEAR}>Test</Button>);
+        expect(screen.getByTestId('Test')).toHaveClass('clear');
+        screen.debug();
+    });
+});

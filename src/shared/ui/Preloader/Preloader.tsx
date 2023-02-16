d4f7617d808e86f64417796310833afe62896095
Yulia Starchenko <imay@amake.ru>
2023-02-16 23:08:13 +0300

    Fixed: Preloader final

--- src/shared/ui/Preloader/Preloader.tsx
+++ src/shared/ui/Preloader/Preloader.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { cn } from 'shared/lib/classNames/classNames';
-import PreloaderIcon from 'shared/assets/icons/Preloader.svg';
 import s from './Preloader.module.scss';
 
 interface PreloaderProps {
@@ -11,9 +10,12 @@ const Preloader: React.FC<PreloaderProps> = (props) => {
     const { className } = props;
 
     return (
-        <PreloaderIcon
-            className={cn(s.preloader, {}, [className])}
-        />
+        <div className={cn(s.preloader, {}, [className])}>
+            <div />
+            <div />
+            <div />
+            <div />
+        </div>
     );
 };
 

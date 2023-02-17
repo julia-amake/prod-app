1a5de74fa72e470eeb8b865fa3bbee3b458bb713
Yulia Starchenko <imay@amake.ru>
2023-02-17 23:23:55 +0300

    Updated: ThemeSwitcher

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@ import { cn } from 'shared/lib/classNames/classNames';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import NightOn from 'shared/assets/icons/NightOn.svg';
 import NightOff from 'shared/assets/icons/NightOff.svg';
-import Button, { ThemeButton } from 'shared/ui/Button/Button';
+import Button, { ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
 import s from './ThemeSwitcher.module.scss';
 
@@ -19,7 +19,7 @@ const ThemeSwitcher: React.FC<ThemeSwitcherProps> = (props) => {
 
     return (
         <Button
-            theme={ThemeButton.CLEAR}
+            theme={ButtonTheme.CLEAR}
             className={cn(s.switcher, {}, [className])}
             onClick={toggleTheme}
             title={t('Переключить тему')}

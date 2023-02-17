1ee977f243978bbdca3106907124069dac87f9b4
Yulia Starchenko <imay@amake.ru>
2023-02-17 23:24:16 +0300

    Created: SidebarToggle

---
+++ src/widgets/Sidebar/ui/Sidebar/SidebarToggle/SidebarToggle.tsx
@@ -0,0 +1,43 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import Button, { ButtonShape, ButtonSize } from 'shared/ui/Button/Button';
+import ArrowRight from 'shared/assets/icons/ArrowRightSimpleLine.svg';
+import ArrowLeft from 'shared/assets/icons/ArrowLeftSimpleLine.svg';
+import { useTranslation } from 'react-i18next';
+import { cn } from 'shared/lib/classNames/classNames';
+import s from './SidebarToggle.module.scss';
+
+interface SidebarToggleProps {
+    collapsed: boolean;
+    setCollapsed: Dispatch<SetStateAction<boolean>>;
+}
+
+const SidebarToggle: React.FC<SidebarToggleProps> = (props) => {
+    const {
+        collapsed,
+        setCollapsed,
+    } = props;
+
+    const { t } = useTranslation();
+
+    const onToggle = () => {
+        setCollapsed((prev) => !prev);
+    };
+
+    return (
+        <Button
+            className={cn(s.sidebarToggle, { [s.collapsed]: collapsed })}
+            title={t(collapsed ? 'Развернуть' : 'Свернуть')}
+            shape={ButtonShape.SQUARE}
+            size={ButtonSize.L}
+            iconOnly
+            onClick={onToggle}
+            data-testid="sidebar-toggle"
+        >
+            {collapsed
+                ? <ArrowRight className={s.icon} />
+                : <ArrowLeft className={s.icon} />}
+        </Button>
+    );
+};
+
+export default SidebarToggle;

996c6cb01755579a15715df1a65d0d99cdc7855f
Yulia Starchenko <imay@amake.ru>
2023-02-17 23:25:35 +0300

    Added: Button new styles and stories

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import Button, { ThemeButton } from 'shared/ui/Button/Button';
+import Button, { ButtonShape, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 
@@ -14,33 +14,291 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const PrimaryRoundedS = Template.bind({});
+PrimaryRoundedS.args = {
     children: 'Button',
-    theme: ThemeButton.PRIMARY,
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.S,
 };
+
+export const PrimaryRoundedSIcon = Template.bind({});
+PrimaryRoundedSIcon.args = {
+    children: '$',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.S,
+    iconOnly: true,
+};
+
+export const PrimaryRoundedM = Template.bind({});
+PrimaryRoundedM.args = {
+    children: 'Button',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.M,
+};
+
+export const PrimaryRoundedMIcon = Template.bind({});
+PrimaryRoundedMIcon.args = {
+    children: '$',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.M,
+    iconOnly: true,
+};
+
+export const PrimaryRoundedL = Template.bind({});
+PrimaryRoundedL.args = {
+    children: 'Button',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.L,
+};
+
+export const PrimaryRoundedLIcon = Template.bind({});
+PrimaryRoundedLIcon.args = {
+    children: '$',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.L,
+    iconOnly: true,
+};
+
+export const PrimarySquaredS = Template.bind({});
+PrimarySquaredS.args = {
+    children: 'Button',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.S,
+};
+
+export const PrimarySquaredSIcon = Template.bind({});
+PrimarySquaredSIcon.args = {
+    children: '$',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.S,
+    iconOnly: true,
+};
+
+export const PrimarySquaredM = Template.bind({});
+PrimarySquaredM.args = {
+    children: 'Button',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.M,
+};
+
+export const PrimarySquaredMIcon = Template.bind({});
+PrimarySquaredMIcon.args = {
+    children: '$',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.M,
+    iconOnly: true,
+};
+
+export const PrimarySquaredL = Template.bind({});
+PrimarySquaredL.args = {
+    children: 'Button',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.L,
+};
+
+export const PrimarySquaredLIcon = Template.bind({});
+PrimarySquaredLIcon.args = {
+    children: '$',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.L,
+    iconOnly: true,
+};
+
 export const PrimaryDark = Template.bind({});
 PrimaryDark.args = {
     children: 'Button',
-    theme: ThemeButton.PRIMARY,
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.M,
 };
 PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
 
-export const Clear = Template.bind({});
-Clear.args = {
+export const PrimaryDarkIcon = Template.bind({});
+PrimaryDarkIcon.args = {
+    children: '$',
+    theme: ButtonTheme.PRIMARY,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.M,
+    iconOnly: true,
+};
+PrimaryDarkIcon.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OutlinedRoundedS = Template.bind({});
+OutlinedRoundedS.args = {
     children: 'Button',
-    theme: ThemeButton.CLEAR,
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.S,
 };
 
-export const Outlined = Template.bind({});
-Outlined.args = {
+export const OutlinedRoundedSIcon = Template.bind({});
+OutlinedRoundedSIcon.args = {
+    children: '$',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.S,
+    iconOnly: true,
+};
+
+export const OutlinedRoundedM = Template.bind({});
+OutlinedRoundedM.args = {
     children: 'Button',
-    theme: ThemeButton.OUTLINED,
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.M,
+};
+
+export const OutlinedRoundedMIcon = Template.bind({});
+OutlinedRoundedMIcon.args = {
+    children: '$',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.M,
+    iconOnly: true,
+};
+
+export const OutlinedRoundedL = Template.bind({});
+OutlinedRoundedL.args = {
+    children: 'Button',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.L,
+};
+
+export const OutlinedRoundedLIcon = Template.bind({});
+OutlinedRoundedLIcon.args = {
+    children: '$',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.L,
+    iconOnly: true,
+};
+
+export const OutlinedSquaredS = Template.bind({});
+OutlinedSquaredS.args = {
+    children: 'Button',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.S,
+};
+
+export const OutlinedSquaredSIcon = Template.bind({});
+OutlinedSquaredSIcon.args = {
+    children: '$',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.S,
+    iconOnly: true,
+};
+
+export const OutlinedSquaredM = Template.bind({});
+OutlinedSquaredM.args = {
+    children: 'Button',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.M,
+};
+
+export const OutlinedSquaredMIcon = Template.bind({});
+OutlinedSquaredMIcon.args = {
+    children: '$',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.M,
+    iconOnly: true,
+};
+
+export const OutlinedSquaredL = Template.bind({});
+OutlinedSquaredL.args = {
+    children: 'Button',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.L,
+};
+
+export const OutlinedSquaredLicon = Template.bind({});
+OutlinedSquaredLicon.args = {
+    children: '$',
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.L,
+    iconOnly: true,
 };
 
 export const OutlinedDark = Template.bind({});
 OutlinedDark.args = {
     children: 'Button',
-    theme: ThemeButton.OUTLINED,
+    theme: ButtonTheme.OUTLINED,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.M,
 };
 OutlinedDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const ClearRoundedS = Template.bind({});
+ClearRoundedS.args = {
+    children: 'Button',
+    theme: ButtonTheme.CLEAR,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.S,
+};
+
+export const ClearRoundedM = Template.bind({});
+ClearRoundedM.args = {
+    children: 'Button',
+    theme: ButtonTheme.CLEAR,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.M,
+};
+
+export const ClearRoundedL = Template.bind({});
+ClearRoundedL.args = {
+    children: 'Button',
+    theme: ButtonTheme.CLEAR,
+    shape: ButtonShape.ROUND,
+    size: ButtonSize.L,
+};
+
+export const ClearSquaredS = Template.bind({});
+ClearSquaredS.args = {
+    children: 'Button',
+    theme: ButtonTheme.CLEAR,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.S,
+};
+
+export const ClearSquaredM = Template.bind({});
+ClearSquaredM.args = {
+    children: 'Button',
+    theme: ButtonTheme.CLEAR,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.M,
+};
+
+export const ClearSquaredL = Template.bind({});
+ClearSquaredL.args = {
+    children: 'Button',
+    theme: ButtonTheme.CLEAR,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.L,
+};
+
+export const ClearDark = Template.bind({});
+ClearDark.args = {
+    children: 'Button',
+    theme: ButtonTheme.CLEAR,
+    shape: ButtonShape.SQUARE,
+    size: ButtonSize.L,
+};
+ClearDark.decorators = [ThemeDecorator(Theme.DARK)];
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,20 +2,37 @@ import { FC, ButtonHTMLAttributes } from 'react';
 import { cn } from 'shared/lib/classNames/classNames';
 import s from './Button.module.scss';
 
-export enum ThemeButton {
+export enum ButtonTheme {
     CLEAR = 'clear',
     PRIMARY = 'primary',
-    OUTLINED = 'outlined'
+    OUTLINED = 'outlined',
+}
+
+export enum ButtonShape {
+    ROUND = 'shape-rounded',
+    SQUARE = 'shape-square'
+}
+
+export enum ButtonSize {
+    S= 'size-s',
+    M= 'size-m',
+    L= 'size-l'
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
-    theme?: ThemeButton;
+    theme?: ButtonTheme;
+    shape?: ButtonShape;
+    size?: ButtonSize,
+    iconOnly?: boolean,
 }
 
 const Button: FC<ButtonProps> = (props) => {
     const {
-        theme = ThemeButton.PRIMARY,
+        theme = ButtonTheme.PRIMARY,
+        shape = ButtonShape.ROUND,
+        size = ButtonSize.L,
+        iconOnly = false,
         className = '',
         children,
         ...otherProps
@@ -25,8 +42,8 @@ const Button: FC<ButtonProps> = (props) => {
         <button
             className={cn(
                 s.button,
-                { [s[theme]]: true },
-                [className],
+                { [s.iconOnly]: iconOnly },
+                [className, s[theme], s[shape], s[size]],
             )}
             type="button"
             /* eslint-disable-next-line react/jsx-props-no-spreading */

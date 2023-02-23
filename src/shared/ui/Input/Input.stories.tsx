f766e840139156a1b97a08c5ad29b741cca3ab2d
Yulia Starchenko <imay@amake.ru>
2023-02-23 21:00:52 +0300

    Added: input story

---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Input from './Input';
+
+export default {
+    title: 'shared/Input',
+    component: Input,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: {},
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Input {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+    label: 'Input',
+    placeholder: 'Placeholder',
+};
+
+export const PrimaryLabelOnly = Template.bind({});
+PrimaryLabelOnly.args = {
+    label: 'Input',
+};
+
+export const PrimaryPlaceholderOnly = Template.bind({});
+PrimaryPlaceholderOnly.args = {
+    placeholder: 'Placeholder',
+};
+
+export const PrimaryEmpty = Template.bind({});

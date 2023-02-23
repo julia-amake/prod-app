50d10bc535b76e2c80232af94c294851c6eea261
Yulia Starchenko <imay@amake.ru>
2023-02-23 21:13:27 +0300

    Removed: mock input

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,24 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import Input from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
-    const [value, setValue] = useState('');
-
-    const onChange = (val: string) => {
-        setValue(val);
-    };
-
     return (
         <div className="main-content">
             {t('Главная страница')}
-            <Input
-                value={value}
-                label={t('Лэйбл')}
-                onChange={onChange}
-            />
         </div>
     );
 };

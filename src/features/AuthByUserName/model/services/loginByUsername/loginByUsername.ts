d46a48eeeb7019850a9cd52aa573930556a866e5
Yulia Starchenko <imay@amake.ru>
2023-02-27 15:09:25 +0300

    Created: User and AuthByUsername rtk with axios

---
+++ src/features/AuthByUserName/model/services/loginByUsername/loginByUsername.ts
@@ -0,0 +1,33 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { User, userActions } from 'entities/User';
+import { USER_LOCALSTORAGE_KEY } from 'shared/consts/localstorage';
+
+interface LoginByUsernameProps {
+    username: string;
+    password: string
+}
+
+export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, {rejectValue: string}>(
+    'login/loginByUsername',
+    async (authData, thunkAPI) => {
+        try {
+            const response = await axios.post<User>('http://localhost:8000/login', authData);
+
+            if (!response.data) {
+                throw new Error();
+            }
+
+            // localStorage только для имитации бекенда, на проде так делать нельзя
+            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
+
+            thunkAPI.dispatch(userActions.setAuthData(response.data));
+
+            return response.data;
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            console.log(e);
+            return thunkAPI.rejectWithValue('error');
+        }
+    },
+);

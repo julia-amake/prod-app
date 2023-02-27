bd6b003d95a499478ee947e2cdafe589fcd4c122
Yulia Starchenko <imay@amake.ru>
2023-02-27 15:11:59 +0300

    Added: initAuthData to App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,20 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { cn } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { ContentLoader } from 'shared/ui';
+import { useDispatch } from 'react-redux';
+import { userActions } from 'entities/User';
 
 function App() {
+    const dispatch = useDispatch();
+
+    // берем юзера из local storage
+    useEffect(() => {
+        dispatch(userActions.initAuthData());
+    }, [dispatch]);
+
     return (
         <div className={cn('app', {}, [])}>
             <Suspense fallback={<ContentLoader />}>

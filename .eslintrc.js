c6a3bb5c2873aaa99518282600bfc864d7437c3c
Yulia Starchenko <imay@amake.ru>
2023-02-28 15:12:06 +0300

    Updated: ts linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,6 +65,7 @@ module.exports = {
         ],
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/no-autofocus': 'warn',
     },
     globals: {
         __IS_DEV__: true,

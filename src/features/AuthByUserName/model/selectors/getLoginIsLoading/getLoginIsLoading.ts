bafa17b590d9668b99a823b7c6c2a2ac41e6dfa0
Yulia Starchenko <imay@amake.ru>
2023-03-01 22:02:42 +0300

    Updated: hard selector initials with initialState

--- src/features/AuthByUserName/model/selectors/getLoginIsLoading/getLoginIsLoading.ts
+++ src/features/AuthByUserName/model/selectors/getLoginIsLoading/getLoginIsLoading.ts
@@ -1,8 +1,9 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { LoginSchema } from 'features/AuthByUserName';
 import { getLoginState } from '../getLoginState/getLoginState';
+import { initialState } from '../../slice/loginSlice';
 
 export const getLoginIsLoading = createSelector(
     getLoginState,
-    (loginForm: LoginSchema) => loginForm?.isLoading || false,
+    (loginForm: LoginSchema) => loginForm?.isLoading || initialState.isLoading,
 );
--- src/features/AuthByUserName/model/selectors/getLoginUsername/getLoginUsername.ts
+++ src/features/AuthByUserName/model/selectors/getLoginUsername/getLoginUsername.ts
@@ -1,8 +1,9 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { LoginSchema } from 'features/AuthByUserName';
 import { getLoginState } from '../getLoginState/getLoginState';
+import { initialState } from '../../slice/loginSlice';
 
 export const getLoginUsername = createSelector(
     getLoginState,
-    (loginForm: LoginSchema) => loginForm?.username || '',
+    (loginForm: LoginSchema) => loginForm?.username || initialState.username,
 );

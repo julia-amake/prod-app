10734a0429df37e7df50d8ecd0865edf2bea489b
Yulia Starchenko <imay@amake.ru>
2023-03-02 21:21:48 +0300

    Added: getLogins tests

---
+++ src/features/AuthByUserName/model/selectors/getLoginState/getLoginState.test.ts
@@ -0,0 +1,34 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginState } from './getLoginState';
+
+describe('getLoginState.test', () => {
+    test('should return login state', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: 'login',
+                password: '111',
+                isLoading: false,
+                error: '',
+            },
+        };
+
+        expect(getLoginState(state as StateSchema))
+            .toEqual({
+                username: 'login',
+                password: '111',
+                isLoading: false,
+                error: '',
+            });
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+
+        expect(getLoginState(state as StateSchema)).toEqual({
+            username: '',
+            password: '',
+            isLoading: false,
+            error: '',
+        });
+    });
+});

7aa1f068c87f1e2854c8ebbaa879a1e263515349
Yulia Starchenko <imay@amake.ru>
2023-03-03 22:51:37 +0300

    Created: useAppDispatch hook

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,12 +1,12 @@
 import type {
     StateSchema,
-    ReducerStoreWithManager,
 } from './config/StateSchema';
-import { createReduxStore } from './config/store';
+import { AppDispatch, createReduxStore } from './config/store';
 import StoreProvider from './ui/StoreProvider';
 
 export {
     StoreProvider,
     createReduxStore,
     StateSchema,
+    AppDispatch,
 };

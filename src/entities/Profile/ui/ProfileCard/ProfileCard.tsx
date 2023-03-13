cbdee263f153499c14117b4b1be2df54d79fbbba
Yulia Starchenko <imay@amake.ru>
2023-03-13 16:17:32 +0300

    Updated: profile card

--- src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -1,59 +1,128 @@
 import React from 'react';
 import { cn } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
-import { getProfileError, getProfileIsLoading } from 'entities/Profile';
-import Heading, { HeadingSize } from 'shared/ui/Heading/Heading';
-import Button, { ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
-import EditLine from 'shared/assets/icons/EditLine.svg';
 import Input from 'shared/ui/Input/Input';
-import { getProfileData } from '../../model/selectors/getProfileData/getProfileData';
-import { getProfileReadonly } from '../../model/selectors/getProfileReadonly/getProfileReadonly';
+import Preloader from 'shared/ui/Preloader/Preloader';
+import Informer from 'shared/ui/Informer/Informer';
+import Avatar from 'shared/ui/Avatar/Avatar';
+import { Currency, CurrencySelect } from 'entities/Currency';
+import { Country, CountrySelect } from 'entities/Country';
+import { Profile } from '../../model/types/profile';
 import s from './ProfileCard.module.scss';
 
 interface ProfileCardProps {
     className?: string;
+    data?: Profile | null;
+    readOnly: boolean;
+    isLoading: boolean;
+    error?: string;
+    onChangeFirstname?: (value: string) => void;
+    onChangeLastname?: (value: string) => void;
+    onChangeAge?: (value: string) => void;
+    onChangeCity?: (value: string) => void;
+    onChangeAvatar?: (value: string) => void;
+    onChangeUsername?: (value: string) => void;
+    onChangeCurrency?: (value: Currency) => void;
+    onChangeCountry?: (value: Country) => void;
 }
 
 export const ProfileCard: React.FC<ProfileCardProps> = (props) => {
     const {
+        data = null,
+        readOnly,
+        isLoading,
         className = '',
+        error = '',
+        onChangeFirstname,
+        onChangeLastname,
+        onChangeAge,
+        onChangeCity,
+        onChangeAvatar,
+        onChangeUsername,
+        onChangeCurrency,
+        onChangeCountry,
     } = props;
 
-    const { t } = useTranslation();
-    const data = useSelector(getProfileData);
-    const readonly = useSelector(getProfileReadonly);
-    const isLoading = useSelector(getProfileIsLoading);
-    const error = useSelector(getProfileError);
+    const { t } = useTranslation(['profile', 'translation']);
 
     return (
         <div className={cn(s.outer, {}, [className])}>
-            <header className={s.header}>
-                <Heading
-                    size={HeadingSize.L}
-                    className={s.title}
-                >
-                    {t('Профиль')}
-                </Heading>
-                <Button
-                    label={t('Редактировать')}
-                    theme={ButtonTheme.OUTLINED}
-                    size={ButtonSize.M}
-                    icon={{ element: EditLine }}
+            {!isLoading && !error && data && (
+                <div className={cn(s.data, {})}>
+                    {data.avatar && (
+                        <div className={s.avatar}>
+                            <Avatar
+                                src={data.avatar}
+                            />
+                        </div>
+                    )}
+                    <div className={s.fields}>
+                        <Input
+                            label={t('Ваше имя')}
+                            value={data?.name}
+                            readOnly={readOnly}
+                            className={s.input}
+                            onChange={onChangeFirstname}
+                        />
+                        <Input
+                            label={t('Ваша фамилия')}
+                            value={data?.lastname || ''}
+                            readOnly={readOnly}
+                            className={s.input}
+                            onChange={onChangeLastname}
+                        />
+                        <Input
+                            label={t('Ваш возраст')}
+                            value={data?.age || ''}
+                            readOnly={readOnly}
+                            className={s.input}
+                            onChange={onChangeAge}
+                        />
+                        <Input
+                            label={t('Ваш город')}
+                            value={data?.city || ''}
+                            readOnly={readOnly}
+                            className={s.input}
+                            onChange={onChangeCity}
+                        />
+                        <Input
+                            label={t('Имя пользователя')}
+                            value={data?.username || ''}
+                            readOnly={readOnly}
+                            className={s.input}
+                            onChange={onChangeUsername}
+                        />
+                        <Input
+                            label={t('Ссылка на аватар')}
+                            value={data?.avatar || ''}
+                            readOnly={readOnly}
+                            className={s.input}
+                            onChange={onChangeAvatar}
+                        />
+                        <CurrencySelect
+                            label={t('Валюта')}
+                            value={data.currency}
+                            onChange={onChangeCurrency}
+                            readOnly={readOnly}
+                            className={s.input}
+                        />
+                        <CountrySelect
+                            label={t('translation:Страна')}
+                            value={data.country}
+                            onChange={onChangeCountry}
+                            readOnly={readOnly}
+                            className={s.input}
+                        />
+                    </div>
+                </div>
+            )}
+            {isLoading && <Preloader />}
+            {error && (
+                <Informer
+                    title={t('Ошибка_при_загрузке_пользователя')}
+                    text={t('translation:Попробуйте_обновить_страницу')}
                 />
-            </header>
-            <div className={s.data}>
-                <Input
-                    label={t('Ваше имя')}
-                    value={data?.name}
-                    className={s.input}
-                />
-                <Input
-                    label={t('Ваша фамилия')}
-                    value={data?.lastname}
-                    className={s.input}
-                />
-            </div>
+            )}
         </div>
     );
 };
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,7 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { ReducersList, useDynamicModuleLoader } from 'shared/lib/hooks/useDynamicModuleLoader/useDynamicModuleLoader';
-import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile';
+import {
+    fetchProfileData,
+    getProfileError,
+    getProfileFormData,
+    getProfileIsLoading,
+    getProfileReadonly,
+    profileActions,
+    ProfileCard,
+    profileReducer,
+} from 'entities/Profile';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { IntegerValidation } from 'shared/lib/utils/validations';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country';
+import ProfilePageHeader from './ProfilePageHeader/ProfilePageHeader';
 
 const initialReducers: ReducersList = {
     profile: profileReducer,
@@ -10,6 +24,10 @@ const initialReducers: ReducersList = {
 interface ProfilePageProps {}
 
 const ProfilePage: React.FC<ProfilePageProps> = () => {
+    const formData = useSelector(getProfileFormData);
+    const readonly = useSelector(getProfileReadonly);
+    const isLoading = useSelector(getProfileIsLoading);
+    const error = useSelector(getProfileError);
     const dispatch = useAppDispatch();
 
     useDynamicModuleLoader(initialReducers, true);
@@ -18,9 +36,98 @@ const ProfilePage: React.FC<ProfilePageProps> = () => {
         dispatch(fetchProfileData());
     }, [dispatch]);
 
+    const onChangeFirstname = useCallback(
+        (value?: string) => {
+            dispatch(profileActions.updateProfile({
+                name: value || '',
+            }));
+        },
+        [dispatch],
+    );
+
+    const onChangeLastname = useCallback(
+        (value?: string) => {
+            dispatch(profileActions.updateProfile({
+                lastname: value || '',
+            }));
+        },
+        [dispatch],
+    );
+
+    const onChangeAge = useCallback(
+        (value: string = '') => {
+            dispatch(profileActions.updateProfile({
+                age: IntegerValidation(value),
+            }));
+        },
+        [dispatch],
+    );
+
+    const onChangeCity = useCallback(
+        (value?: string) => {
+            dispatch(profileActions.updateProfile({
+                city: value || '',
+            }));
+        },
+        [dispatch],
+    );
+
+    const onChangeUsername = useCallback(
+        (value?: string) => {
+            dispatch(profileActions.updateProfile({
+                username: value || '',
+            }));
+        },
+        [dispatch],
+    );
+
+    const onChangeAvatar = useCallback(
+        (value?: string) => {
+            dispatch(profileActions.updateProfile({
+                avatar: value || '',
+            }));
+        },
+        [dispatch],
+    );
+
+    const onChangeCurrency = useCallback(
+        (value?: Currency) => {
+            dispatch(profileActions.updateProfile({
+                currency: value || Currency.RUB,
+            }));
+        },
+        [dispatch],
+    );
+
+    const onChangeCountry = useCallback(
+        (value?: Country) => {
+            dispatch(profileActions.updateProfile({
+                country: value || Country.RUSSIA,
+            }));
+        },
+        [dispatch],
+    );
+
     return (
         <div className="main-content">
-            <ProfileCard />
+            <ProfilePageHeader
+                readonly={readonly}
+                isLoading={isLoading}
+            />
+            <ProfileCard
+                data={formData}
+                isLoading={isLoading}
+                error={error}
+                readOnly={readonly}
+                onChangeFirstname={onChangeFirstname}
+                onChangeLastname={onChangeLastname}
+                onChangeAge={onChangeAge}
+                onChangeCity={onChangeCity}
+                onChangeUsername={onChangeUsername}
+                onChangeAvatar={onChangeAvatar}
+                onChangeCurrency={onChangeCurrency}
+                onChangeCountry={onChangeCountry}
+            />
         </div>
     );
 };

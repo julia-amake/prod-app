f50a455a197fef9b9dc4bb588422d32a424736e2
Yulia Starchenko <imay@amake.ru>
2023-03-13 16:16:12 +0300

    Created: currency entity

---
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
@@ -0,0 +1,41 @@
+import React, { FC, memo, useCallback } from 'react';
+import { cn } from 'shared/lib/classNames/classNames';
+import Select, { SelectOption } from 'shared/ui/Select/Select';
+import { Currency } from '../../model/types/currency';
+
+interface CurrencySelectProps {
+    className?: string;
+    value?: Currency;
+    onChange?: (value: Currency) => void;
+    label?: string;
+    readOnly?: boolean;
+}
+
+const options:SelectOption[] = Object.values(Currency).map((item) => ({
+    value: item, content: item,
+}));
+
+export const CurrencySelect: FC<CurrencySelectProps> = memo((props) => {
+    const {
+        className = '',
+        value = Currency.RUB,
+        label = '',
+        onChange,
+        readOnly = false,
+    } = props;
+
+    const onChangeHandler = useCallback((value: string) => {
+        onChange?.(value as Currency);
+    }, [onChange]);
+
+    return (
+        <Select
+            className={cn('', {}, [className])}
+            label={label}
+            options={options}
+            value={value}
+            onChange={onChangeHandler}
+            readOnly={readOnly}
+        />
+    );
+});

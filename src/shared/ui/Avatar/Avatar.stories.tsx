770cd5b75dc2f54f435edb907345e6e45b8e2e2e
Yulia Starchenko <imay@amake.ru>
2023-03-15 21:00:23 +0300

    Update: avatar import in story

--- src/shared/ui/Avatar/Avatar.stories.tsx
+++ src/shared/ui/Avatar/Avatar.stories.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import avatarSrc from 'shared/assets/tests/avatar.jpeg';
 import Avatar from './Avatar';
-import avatarSrc from './avatar.jpeg';
 
 export default {
     title: 'shared/Avatar',

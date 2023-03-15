ba7e59fa832c031c0b81e3850dc88bde9e4d1537
Yulia Starchenko <imay@amake.ru>
2023-03-15 21:07:02 +0300

    Fixed: replaced formData selector with getState

--- src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
+++ src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
@@ -1,14 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
-import { validateProfileData } from 'entities/Profile/model/serveces/validateProfileData/validateProfileData';
+import { validateProfileData } from '../validateProfileData/validateProfileData';
 import { Profile, ValidateProfileError } from '../../types/profile';
-import { getProfileFormData } from '../../selectors/getProfileFormData/getProfileFormData';
 
 export const updateProfileData = createAsyncThunk<Profile, void, ThunkConfig<ValidateProfileError[]>>(
     'profile/updateProfileData',
     async (_, thunkAPI) => {
         const { getState, rejectWithValue, extra } = thunkAPI;
-        const formData = getProfileFormData(getState());
+        const formData = getState().profile?.formData;
         const errors = validateProfileData(formData);
 
         if (errors.length > 0) {
@@ -17,6 +16,11 @@ export const updateProfileData = createAsyncThunk<Profile, void, ThunkConfig<Val
 
         try {
             const response = await extra.api.put<Profile>('/profile', formData);
+
+            if (!response.data) {
+                throw new Error();
+            }
+
             return response.data;
         } catch (e) {
             return rejectWithValue([ValidateProfileError.SERVER_ERROR]);

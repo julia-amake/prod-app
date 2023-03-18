8553a8502ef47b7bd724d07e11648da7b0c8b8d0
Yulia Starchenko <imay@amake.ru>
2023-03-18 10:12:37 +0300

    Created: icon component

---
+++ src/shared/ui/Icon/Icon.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import { cn } from 'shared/lib/classNames/classNames';
+import s from './Icon.module.scss';
+
+interface IconProps {
+    svg: React.VFC<React.SVGProps<SVGSVGElement>>;
+    className?: string;
+}
+
+const Icon = memo((props: IconProps) => {
+    const {
+        svg,
+        className = '',
+    } = props;
+
+    const Svg = svg;
+
+    return (
+        <Svg className={cn(s.icon, {}, [className])} />
+    );
+});
+
+export default Icon;

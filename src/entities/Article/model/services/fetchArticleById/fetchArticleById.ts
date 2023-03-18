3542a75e7003df6aa5eaa141e56d1d1624f39081
Yulia Starchenko <imay@amake.ru>
2023-03-18 10:43:40 +0300

    Created: async thunk for get article

---
+++ src/entities/Article/model/services/fetchArticleById/fetchArticleById.ts
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ThunkConfig } from 'app/providers/StoreProvider';
+import { Article } from '../../types/article';
+
+export const fetchArticleById = createAsyncThunk<
+    Article,
+    string, // id
+    ThunkConfig<string>
+    >(
+        'article/fetchArticleById',
+        async (id, thunkAPI) => {
+            const { rejectWithValue, extra } = thunkAPI;
+
+            try {
+                const response = await extra.api.get<Article>(`/articles/${id}`);
+                // fake error
+                if (!response.data) {
+                    throw new Error();
+                }
+
+                return response.data;
+            } catch (e) {
+                return rejectWithValue('error');
+            }
+        },
+    );

c24bada32c4e0ea8fde9ef152069ca2ddaed886c
Yulia Starchenko <imay@amake.ru>
2023-03-18 10:49:31 +0300

    Created: article details page

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,7 +1,9 @@
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
 import { cn } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import s from './ArticleDetailsPage.module.scss';
+import { ArticleDetails } from 'entities/Article';
+import { useParams } from 'react-router-dom';
+import Informer from 'shared/ui/Informer/Informer';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -13,10 +15,16 @@ const ArticleDetailsPage = memo((props: ArticleDetailsPageProps) => {
     } = props;
 
     const { t } = useTranslation('article');
+    const { id } = useParams();
+
+    const content = useMemo(() => {
+        if (!id) return <Informer title={t('Статья не найдена')} />;
+        return <ArticleDetails id={id} />;
+    }, [t, id]);
 
     return (
-        <div className={cn(s.outer, {}, [className])}>
-            ARTICLE DETAILS PAGE
+        <div className={cn('main-content', {}, [className])}>
+            {content}
         </div>
     );
 });

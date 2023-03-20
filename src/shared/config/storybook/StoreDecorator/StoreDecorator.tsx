53073dfacfaa46c2ae5ab80bc2317985066e4a4a
Yulia Starchenko <imay@amake.ru>
2023-03-20 14:14:31 +0300

    Added: articleDetails to reducers

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -4,10 +4,12 @@ import { ReducersMapObject } from '@reduxjs/toolkit';
 import { loginReducer } from 'features/AuthByUserName/model/slice/loginSlice';
 import { profileReducer } from 'entities/Profile';
 import { ReducersList } from 'shared/lib/hooks/useDynamicModuleLoader/useDynamicModuleLoader';
+import { ArticleDetailsReducer } from 'entities/Article';
 
 const defaultAsyncReducers: ReducersList = {
     loginForm: loginReducer,
     profile: profileReducer,
+    articleDetails: ArticleDetailsReducer,
 };
 
 export const StoreDecorator = (

39fd84fd93e7a747d1f6a5eeb74fc8b592efc0d2
Yulia Starchenko <imay@amake.ru>
2023-03-20 14:14:07 +0300

    Created: Code component

---
+++ src/shared/ui/Code/Code.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Code } from './Code';
+
+export default {
+    title: 'shared/Code',
+    component: Code,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: {
+        content: 'export default {\n'
+            + '    title: \'shared/Code\',\n'
+            + '    component: Code,\n'
+            + '    argTypes: {\n'
+            + '        backgroundColor: { control: \'color\' },\n'
+            + '    },\n'
+            + '    args: {},\n'
+            + '} as ComponentMeta<typeof Code>;',
+    },
+} as ComponentMeta<typeof Code>;
+
+const Template: ComponentStory<typeof Code> = (args) => (
+    <Code {...args} />
+);
+
+export const Normal = Template.bind({});
+Normal.args = {};
---
+++ src/shared/ui/Code/Code.tsx
@@ -0,0 +1,51 @@
+import React, {
+    memo, useCallback, useState,
+} from 'react';
+import { cn } from 'shared/lib/classNames/classNames';
+import Button, { ButtonTheme } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import CopyLine from 'shared/assets/icons/CopyLine.svg';
+import CopyDoneLine from 'shared/assets/icons/CopyDoneLine.svg';
+import s from './Code.module.scss';
+
+interface CodeProps {
+    content: string;
+    className?: string;
+}
+
+export const Code = memo((props: CodeProps) => {
+    const {
+        content,
+        className = '',
+    } = props;
+
+    const { t } = useTranslation();
+    const [isCopied, setIsCopied] = useState(false);
+
+    const onCopy = useCallback(
+        () => {
+            navigator.clipboard.writeText(content).then(() => {
+                setIsCopied(true);
+            });
+        },
+        [content],
+    );
+
+    return (
+        <pre className={cn(s.outer, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR}
+                icon={{
+                    element: isCopied ? CopyDoneLine : CopyLine,
+                    className: cn(s.copy_icon, { [s.copy_icon_done]: isCopied }),
+                }}
+                className={s.copy}
+                title={t(isCopied ? 'Скопировано' : 'Копировать')}
+                onClick={onCopy}
+            />
+            <code>
+                {content}
+            </code>
+        </pre>
+    );
+});

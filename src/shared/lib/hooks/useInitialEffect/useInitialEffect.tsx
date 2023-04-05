616e74aa25c1e8442b311e5a678d3c5af6ca1db9
Yulia Starchenko <imay@amake.ru>
2023-04-05 15:00:05 +0300

    Added: deps to useInitialEffect

--- src/shared/lib/hooks/useInitialEffect/useInitialEffect.tsx
+++ src/shared/lib/hooks/useInitialEffect/useInitialEffect.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 
-export function useInitialEffect(callback: () => void) {
+export function useInitialEffect(callback: () => void, deps: unknown[] = []) {
     useEffect(() => {
         if (__PROJECT__ === 'storybook') return;
         callback();
         // eslint-disable-next-line
-    }, []);
+    }, deps);
 }

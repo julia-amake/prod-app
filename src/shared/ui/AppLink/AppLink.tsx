49c9f9e02752b41eb4ea660da238e302d2b6ac24
Yulia Starchenko <imay@amake.ru>
2023-04-05 15:02:37 +0300

    Added: clear theme to AppLink

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -5,7 +5,8 @@ import s from './AppLink.module.scss';
 
 export enum AppLinkTheme {
     PRIMARY = 'primary',
-    SECONDARY = 'secondary'
+    SECONDARY = 'secondary',
+    CLEAR = 'clear'
 }
 
 interface AppLinkProps extends LinkProps {

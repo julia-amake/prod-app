83f04619649e40afad3f82510ddf0dc8df0fd071
Yulia Starchenko <imay@amake.ru>
2023-04-12 14:00:00 +0300

    Added: types for component icons

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,1 +1,14 @@
+import React from 'react';
+
 export type SortOrder = 'asc' | 'desc';
+
+export interface ItemIcon {
+    element: React.VFC<React.SVGProps<SVGSVGElement>>;
+    className?: string;
+}
+
+type ItemIconHPosition = 'left' | 'right' | 'center';
+
+export interface ItemIconWithHPosition extends ItemIcon {
+    position: ItemIconHPosition
+}

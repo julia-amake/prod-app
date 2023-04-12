712ba7ccd9854f7aa79af1a79b4047506c1e63c3
Yulia Starchenko <imay@amake.ru>
2023-04-12 22:12:32 +0300

    Refactored: article details comments

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
@@ -0,0 +1,66 @@
+import React, { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { AddCommentForm } from 'features/AddCommentForm';
+import { CommentList } from 'entities/Comment';
+import { PageSection } from 'shared/ui/PageSection/PageSection';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useSelector } from 'react-redux';
+import Preloader from 'shared/ui/Preloader/Preloader';
+import { getArticleComments } from '../../model/slice/articleDetailsCommentsSlice';
+import { getArticleCommentsError, getArticleCommentsIsLoading } from '../../model/selectors/comments';
+import { addCommentForArticle } from '../../model/services/addCommentForArticle/addCommentForArticle';
+import s from './ArticleDetailsComments.module.scss';
+import { fetchCommentsBrArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+
+interface ArticleDetailsCommentsProps {
+    id: string;
+    className?: string;
+    isLoading: boolean;
+}
+
+export const ArticleDetailsComments = memo((props: ArticleDetailsCommentsProps) => {
+    const {
+        id,
+        isLoading,
+        className = '',
+    } = props;
+
+    const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+
+    const comments = useSelector(getArticleComments.selectAll);
+    const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
+    const error = useSelector(getArticleCommentsError);
+
+    useInitialEffect(() => {
+        dispatch(fetchCommentsBrArticleId(id));
+    }, [id]);
+
+    const onSendComment = useCallback(
+        (text: string) => {
+            dispatch(addCommentForArticle(text));
+        },
+        [dispatch],
+    );
+
+    if (commentsIsLoading || isLoading) return <Preloader />;
+    if (error) return null;
+
+    return (
+        <PageSection
+            className={className}
+            title={t('Комментарии')}
+        >
+            <AddCommentForm
+                onSendComment={onSendComment}
+                className={s.form}
+                isLoading={commentsIsLoading || isLoading}
+            />
+            <CommentList
+                comments={comments}
+                isLoading={commentsIsLoading || isLoading}
+            />
+        </PageSection>
+    );
+});
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,28 +1,16 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ArticleDetails } from 'entities/Article';
 import { useParams } from 'react-router-dom';
 import Informer from 'shared/ui/Informer/Informer';
-import { CommentList } from 'entities/Comment';
 import { ReducersList, useDynamicModuleLoader } from 'shared/lib/hooks/useDynamicModuleLoader/useDynamicModuleLoader';
 import { useSelector } from 'react-redux';
 import { getArticleDetailsIsLoading } from 'entities/Article/model/selectors/articleDetails';
-import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
-import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { AddCommentForm } from 'features/AddCommentForm';
 import { Page } from 'widgets/Page/Page';
 import { ArticleRecommendationsList } from 'features/articleRecommendationsList';
-import { PageSection } from 'shared/ui/PageSection/PageSection';
+import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
 import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
 import { articleDetailsPageReducers } from '../../model/slice';
-import {
-    fetchArticleRecommendations,
-} from '../../model/services/FetchArticleRecommendations/FetchArticleRecommendations';
-import { fetchCommentsBrArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
-import { addCommentForArticle } from '../../model/services/addCommentForArticle/addCommentForArticle';
-import { getArticleComments } from '../../model/slice/articleDetailsCommentsSlice';
-import { getArticleCommentsError, getArticleCommentsIsLoading } from '../../model/selectors/comments';
-import s from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -37,26 +25,10 @@ const ArticleDetailsPage = memo((props: ArticleDetailsPageProps) => {
 
     const { t } = useTranslation('article');
     const { id } = useParams();
-    const dispatch = useAppDispatch();
 
     useDynamicModuleLoader(reducersList, true);
 
     const isLoading = useSelector(getArticleDetailsIsLoading);
-    const comments = useSelector(getArticleComments.selectAll);
-    const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
-    const commentsError = useSelector(getArticleCommentsError);
-
-    useInitialEffect(() => {
-        dispatch(fetchCommentsBrArticleId(id));
-        dispatch(fetchArticleRecommendations());
-    }, [id]);
-
-    const onSendComment = useCallback(
-        (text: string) => {
-            dispatch(addCommentForArticle(text));
-        },
-        [dispatch],
-    );
 
     if (!id) {
         return (
@@ -72,21 +44,7 @@ const ArticleDetailsPage = memo((props: ArticleDetailsPageProps) => {
             className={className}
         >
             <ArticleDetails id={id} isLoading={isLoading} />
-            {!commentsError && (
-                <PageSection
-                    title={t('Комментарии')}
-                >
-                    <AddCommentForm
-                        onSendComment={onSendComment}
-                        className={s.comments_form}
-                        isLoading={commentsIsLoading || isLoading}
-                    />
-                    <CommentList
-                        comments={comments}
-                        isLoading={commentsIsLoading || isLoading}
-                    />
-                </PageSection>
-            )}
+            <ArticleDetailsComments id={id} isLoading={isLoading} />
             <ArticleRecommendationsList />
         </Page>
     );

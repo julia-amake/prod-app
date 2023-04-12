0cbca1befbd7daf698aebb5dc34a0d2fd2e78a9b
Yulia Starchenko <imay@amake.ru>
2023-04-12 22:11:55 +0300

    Moved: useEffect

--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
@@ -3,6 +3,11 @@ import { useTranslation } from 'react-i18next';
 import { ArticleList } from 'entities/Article';
 import { PageSection } from 'shared/ui/PageSection/PageSection';
 import Preloader from 'shared/ui/Preloader/Preloader';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import {
+    fetchArticleRecommendations,
+} from 'pages/ArticleDetailsPage/model/services/FetchArticleRecommendations/FetchArticleRecommendations';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useArticleRecommendationsList } from '../../api/articleRecommendationsApi';
 
 interface ArticleRecommendationsListProps {
@@ -15,6 +20,11 @@ export const ArticleRecommendationsList = memo((props: ArticleRecommendationsLis
     } = props;
     const { t } = useTranslation();
     const { data: articles, isLoading, error } = useArticleRecommendationsList(3);
+    const dispatch = useAppDispatch();
+
+    useInitialEffect(() => {
+        dispatch(fetchArticleRecommendations());
+    }, []);
 
     if (isLoading) return <Preloader />;
     if (error) return null;

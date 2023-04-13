d6f4cd9e39409e9ba092c78d084529af8744e414
Yulia Starchenko <imay@amake.ru>
2023-04-13 21:55:48 +0300

    Added: types to recommendations

--- src/features/articleRecommendationsList/api/articleRecommendationsApi.ts
+++ src/features/articleRecommendationsList/api/articleRecommendationsApi.ts
@@ -1,8 +1,9 @@
 import { rtkApi } from 'shared/api/rtkApi';
+import { Article } from 'entities/Article';
 
 const recommendationsApi = rtkApi.injectEndpoints({
     endpoints: (build) => ({
-        getArticleRecommendationsList: build.query({
+        getArticleRecommendationsList: build.query<Article[], {limit: number, excluded: string}>({
             query: ({ limit, excluded }) => ({
                 url: '/articles',
                 params: {

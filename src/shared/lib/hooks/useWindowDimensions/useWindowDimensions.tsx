a3e6e5ab4480c080a7a9e37caec36f994de76421
Yulia Starchenko <imay@amake.ru>
2023-04-22 13:00:36 +0300

    Created: window dimensions check hook

---
+++ src/shared/lib/hooks/useWindowDimensions/useWindowDimensions.tsx
@@ -0,0 +1,28 @@
+import { useCallback, useEffect, useState } from 'react';
+
+interface useWindowDimensionsResult {
+    width: number;
+    height: number;
+}
+
+const useWindowDimensions = (): useWindowDimensionsResult => {
+    const [width, setWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
+    const [height, setHeight] = useState(typeof window !== 'undefined' ? window.innerHeight : 0);
+
+    const updateWidthAndHeight = useCallback(() => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('resize', updateWidthAndHeight);
+        return () => window.removeEventListener('resize', updateWidthAndHeight);
+    });
+
+    return {
+        width,
+        height,
+    };
+};
+
+export default useWindowDimensions;

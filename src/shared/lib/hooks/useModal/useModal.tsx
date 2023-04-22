40828b71153991731e7d15cacf7f9a3a5db2f2de
Yulia Starchenko <imay@amake.ru>
2023-04-22 16:37:49 +0300

    Created: useModal hook

---
+++ src/shared/lib/hooks/useModal/useModal.tsx
@@ -0,0 +1,69 @@
+import {
+    useCallback, useEffect, useRef, useState,
+} from 'react';
+
+interface useModalProps {
+    onClose?: () => void;
+    isOpen?: boolean;
+    animationDelay?: number;
+}
+
+export const useModal = (props: useModalProps) => {
+    const {
+        isOpen = false,
+        animationDelay = 300,
+        onClose,
+    } = props;
+
+    const [isMounted, setIsMounted] = useState(false);
+    // Нужны для плавного закрытия модалки
+    const [isClosing, setIsClosing] = useState(false);
+    const [isShown, setIsShown] = useState(false);
+
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true);
+            timerRef.current = setTimeout(() => {
+                setIsShown(true);
+            }, 0);
+        }
+    }, [isOpen]);
+
+    const close = useCallback(() => {
+        if (onClose) {
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+                setIsShown(false);
+            }, animationDelay);
+        }
+    }, [animationDelay, onClose]);
+
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            close();
+        }
+    }, [close]);
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+
+        // Делаем очистки
+        return () => {
+            clearTimeout(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+
+    return {
+        isMounted,
+        isClosing,
+        isShown,
+        close,
+    };
+};

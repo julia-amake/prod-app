f807c1356ea6bed1a0d3a92de9c7471c63063dd4
Yulia Starchenko <imay@amake.ru>
2023-04-23 15:04:04 +0300

    Created: dynamic libs loading with AnimationProvider

---
+++ src/shared/lib/components/AnimationProvider/AnimationProvider.tsx
@@ -0,0 +1,47 @@
+import {
+    createContext, ReactNode, useContext, useEffect, useMemo, useRef, useState,
+} from 'react';
+
+type SpringType = typeof import('@react-spring/web');
+type GestureType = typeof import('@use-gesture/react');
+
+interface AnimationContextPayload {
+    Gesture?: GestureType;
+    Spring?: SpringType;
+    isLoaded?: boolean;
+}
+
+const AnimationContext = createContext<AnimationContextPayload>({});
+
+const getAsyncAnimationModules = () => Promise.all([
+    import('@react-spring/web'),
+    import('@use-gesture/react'),
+]);
+
+export const useAnimationLibs = () => useContext(AnimationContext) as Required<AnimationContextPayload>;
+
+export const AnimationProvider = ({ children }: {children: ReactNode}) => {
+    const SpringRef = useRef<SpringType>();
+    const GestureRef = useRef<GestureType>();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const providerValue = useMemo(() => ({
+        Gesture: GestureRef.current,
+        Spring: SpringRef.current,
+        isLoaded,
+    }), [isLoaded]);
+
+    useEffect(() => {
+        getAsyncAnimationModules().then(([Spring, Gesture]) => {
+            SpringRef.current = Spring;
+            GestureRef.current = Gesture;
+            setIsLoaded(true);
+        });
+    }, []);
+
+    return (
+        <AnimationContext.Provider value={providerValue}>
+            {children}
+        </AnimationContext.Provider>
+    );
+};

cc7144e162f9ec2f31ea3d40b2434c94fd6d6cfd
Yulia Starchenko <imay@amake.ru>
2023-04-23 22:11:50 +0300

    Created: updateImports script to change aliases

---
+++ scripts/updateImports.ts
@@ -0,0 +1,25 @@
+import { Project } from 'ts-morph';
+
+const project = new Project();
+project.addSourceFilesAtPaths('src/**/*.ts');
+project.addSourceFilesAtPaths('src/**/*.tsx');
+
+function isAbsolutePath(value: string) {
+    const layers = ['app', 'pages', 'layouts', 'widgets', 'features', 'entities', 'shared'];
+
+    return layers.some((layer) => value.startsWith(layer));
+}
+
+const files = project.getSourceFiles();
+
+files.forEach((sourceFile) => {
+    const importDeclarations = sourceFile.getImportDeclarations();
+    importDeclarations.forEach((importDeclaration) => {
+        const value = importDeclaration.getModuleSpecifierValue();
+        if (isAbsolutePath(value)) {
+            importDeclaration.setModuleSpecifier(`@/${value}`);
+        }
+    });
+});
+
+project.save();

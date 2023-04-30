2e2c8aa16b7b59f9ad28af98c627a760e7028634
Yulia Starchenko <imay@amake.ru>
2023-04-30 13:48:02 +0300

    Updated: custom npm plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,7 +69,7 @@ module.exports = {
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'jsx-a11y/no-autofocus': 'warn',
-        'amake-plugin/path-checker': 'error',
+        'amake-plugin/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,

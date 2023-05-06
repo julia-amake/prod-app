048aa081853050b82f6a6eb1d2c6bedb5ce53845
Yulia Starchenko <imay@amake.ru>
2023-05-06 21:13:39 +0300

    Created: AppImage with fallbacks

---
+++ src/shared/ui/AppImage/AppImage.tsx
@@ -0,0 +1,50 @@
+import React, {
+    ImgHTMLAttributes, memo, ReactElement, useLayoutEffect, useState,
+} from 'react';
+
+interface AppImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+    src: string;
+    alt?: string;
+    fallback?: ReactElement;
+    errorFallback?: ReactElement;
+    className?: string;
+}
+
+export const AppImage = memo((props: AppImageProps) => {
+    const {
+        src,
+        alt = '',
+        fallback,
+        errorFallback,
+        className = '',
+        ...otherProps
+    } = props;
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState(false);
+
+    useLayoutEffect(() => {
+        const img = new Image();
+        img.src = src ?? '';
+        img.onload = () => {
+            setIsLoading(false);
+            setHasError(false);
+        };
+        img.onerror = () => {
+            setIsLoading(false);
+            setHasError(true);
+        };
+    }, [src]);
+
+    if (isLoading && fallback) {
+        return fallback;
+    }
+
+    if (hasError && errorFallback) {
+        return errorFallback;
+    }
+
+    return (
+        <img src={src} className={className} alt={alt} {...otherProps} />
+    );
+});

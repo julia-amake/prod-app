38319b498060cb47287c87e87cb0e24f9cfa4345
Yulia Starchenko <imay@amake.ru>
2023-05-08 16:24:30 +0300

    Added: ArticlesInfiniteList story

--- src/pages/ArticlesPage/ui/ArticlesInfiniteList/ArticlesInfiniteList.stories.tsx
+++ src/pages/ArticlesPage/ui/ArticlesInfiniteList/ArticlesInfiniteList.stories.tsx
@@ -1,9 +1,10 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
+import { RouterDecorator } from '@/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { ArticleSortField, ArticleType, ArticleView } from '@/entities/Article';
+import { article } from '@/entities/Article/testing';
 import { ArticlesInfiniteList } from './ArticlesInfiniteList';
-
-// todo: Вывести данные
 
 export default {
     title: 'pages/ArticlesPage/ArticlesInfiniteList',
@@ -12,6 +13,27 @@ export default {
         backgroundColor: { control: 'color' },
     },
     args: {},
+    decorators: [
+        StoreDecorator({
+            articlesPage: {
+                _isInitialized: true,
+                page: 1,
+                limit: 6,
+                ids: ['1', '2', '3'],
+                entities: {
+                    1: article,
+                    2: { ...article, id: '2' },
+                    3: { ...article, id: '3' },
+                },
+                type: ArticleType.ALL,
+                sort: ArticleSortField.TITLE,
+                view: ArticleView.GRID,
+                order: 'asc',
+                search: '',
+            },
+        }),
+        RouterDecorator('/articles'),
+    ],
 } as ComponentMeta<typeof ArticlesInfiniteList>;
 
 const Template: ComponentStory<typeof ArticlesInfiniteList> = (args) => (
@@ -20,6 +42,3 @@ const Template: ComponentStory<typeof ArticlesInfiniteList> = (args) => (
 
 export const Normal = Template.bind({});
 Normal.args = {};
-Normal.decorators = [StoreDecorator({
-
-})];

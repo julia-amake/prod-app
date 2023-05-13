fb7aeb1f05a5bf2f274d22cdcc0d991c6bb49134
Yulia Starchenko <imay@amake.ru>
2023-05-13 16:49:28 +0300

    Added: isolated tests for components with cypress

---
+++ cypress/component/EditableProfileCard.cy.tsx
@@ -0,0 +1,26 @@
+import { TestProvider } from '@/shared/lib/tests/componentRender/componentRender';
+import { EditableProfileCard } from '@/features/editableProfileCard';
+
+const USER_ID = '4';
+
+describe('EditableProfileCard.cy.tsx', () => {
+    it('playground', () => {
+        cy.intercept('GET', '**/profile/*', { fixture: 'profile.json' });
+        cy.mount(
+            <TestProvider
+                options={{
+                    initialState: {
+                        user: {
+                            authData: {
+                                id: USER_ID,
+                            },
+                        },
+                    },
+                }}
+            >
+                <EditableProfileCard id={USER_ID} />
+            </TestProvider>,
+        );
+        // описываем тест-кейсы
+    });
+});

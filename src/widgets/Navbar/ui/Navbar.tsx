12487b1781dc0d79042f817c8976bde27a95ead9
Yulia Starchenko <imay@amake.ru>
2023-05-18 21:33:28 +0300

    Added: toggle features to Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getRouteArticleCreate } from '@/shared/consts/router';
 import { cn } from '@/shared/lib/classNames/classNames';
+import { ToggleFeatures } from '@/shared/lib/features';
 import { Button, ButtonSize } from '@/shared/ui/Button';
 import { HStack } from '@/shared/ui/Stack';
 import { getUserAuthData } from '@/entities/User';
@@ -37,48 +38,44 @@ const Navbar = memo((props: NavbarProps) => {
 
     if (authData) {
         return (
-            <header className={cn(s.navbar, {}, [className])}>
-                <HStack
-                    className={s.container}
-                    justify="between"
-                    align="center"
-                    gap="16"
-                >
-                    <Button
-                        label={t('Добавить статью')}
-                        size={ButtonSize.M}
-                        onClick={onAddArticle}
-                        type="button"
-                    />
-                    <HStack align="center" gap="24">
+            <ToggleFeatures
+                feature="isAppRedesigned"
+                on={
+                    <HStack className={cn(s.navbarRedesigned, {}, [className])} as="header" gap="16">
                         <NotificationButton />
                         <UserDropdown setIsAuthModal={setIsAuthModal} />
                     </HStack>
-                </HStack>
-            </header>
+                }
+                off={
+                    <header className={cn(s.navbar, {}, [className])}>
+                        <HStack className={s.container} justify="between" align="center" gap="16">
+                            <Button
+                                label={t('Добавить статью')}
+                                size={ButtonSize.M}
+                                onClick={onAddArticle}
+                                type="button"
+                            />
+                            <HStack align="center" gap="24">
+                                <NotificationButton />
+                                <UserDropdown setIsAuthModal={setIsAuthModal} />
+                            </HStack>
+                        </HStack>
+                    </header>
+                }
+            />
         );
     }
 
     return (
         <header className={cn(s.navbar, {}, [className])}>
             <HStack
-                className={cn(s.container, {}, [
-                    'container',
-                    'container_size_l',
-                ])}
+                className={cn(s.container, {}, ['container', 'container_size_l'])}
                 justify="end"
                 align="center"
                 gap="16"
             >
-                <Button
-                    label={t('Войти')}
-                    size={ButtonSize.M}
-                    onClick={onOpenModal}
-                    type="button"
-                />
-                {isAuthModal && (
-                    <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
-                )}
+                <Button label={t('Войти')} size={ButtonSize.M} onClick={onOpenModal} type="button" />
+                {isAuthModal && <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />}
             </HStack>
         </header>
     );

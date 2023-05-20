123e78ffbffa9fc62e87dc4835297fd37633962c
Yulia Starchenko <imay@amake.ru>
2023-05-20 22:39:09 +0300

    Added: toggle features to navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -41,14 +41,23 @@ const Navbar = memo((props: NavbarProps) => {
             <ToggleFeatures
                 feature="isAppRedesigned"
                 on={
-                    <HStack className={cn(s.navbarRedesigned, {}, [className])} as="header" gap="16">
+                    <HStack
+                        className={cn(s.navbarRedesigned, {}, [className])}
+                        as="header"
+                        gap="16"
+                    >
                         <NotificationButton />
                         <UserDropdown setIsAuthModal={setIsAuthModal} />
                     </HStack>
                 }
                 off={
                     <header className={cn(s.navbar, {}, [className])}>
-                        <HStack className={s.container} justify="between" align="center" gap="16">
+                        <HStack
+                            className={s.container}
+                            justify="between"
+                            align="center"
+                            gap="16"
+                        >
                             <Button
                                 label={t('Добавить статью')}
                                 size={ButtonSize.M}
@@ -69,13 +78,23 @@ const Navbar = memo((props: NavbarProps) => {
     return (
         <header className={cn(s.navbar, {}, [className])}>
             <HStack
-                className={cn(s.container, {}, ['container', 'container_size_l'])}
+                className={cn(s.container, {}, [
+                    'container',
+                    'container_size_l',
+                ])}
                 justify="end"
                 align="center"
                 gap="16"
             >
-                <Button label={t('Войти')} size={ButtonSize.M} onClick={onOpenModal} type="button" />
-                {isAuthModal && <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />}
+                <Button
+                    label={t('Войти')}
+                    size={ButtonSize.M}
+                    onClick={onOpenModal}
+                    type="button"
+                />
+                {isAuthModal && (
+                    <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
+                )}
             </HStack>
         </header>
     );

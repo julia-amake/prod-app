76ed6eea9e41c1700ea561b56316451402c8495d
Yulia Starchenko <imay@amake.ru>
2023-05-23 23:21:55 +0300

    Updated: redesigned popovers

---
+++ src/shared/ui/redesigned/Popups/ui/index.ts
@@ -0,0 +1,4 @@
+export { Popover } from './Popover/Popover';
+export { Dropdown } from './Dropdown/Dropdown';
+export { ListBox } from './ListBox/ListBox';
+export type { ListBoxOption } from './ListBox/ListBox';

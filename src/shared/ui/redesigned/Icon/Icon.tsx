80b775e4428af88e5e132a73332ac9f4a72d083f
Yulia Starchenko <imay@amake.ru>
2023-05-23 23:22:35 +0300

    Updated: redesigned Icon

---
+++ src/shared/ui/redesigned/Icon/Icon.tsx
@@ -0,0 +1,24 @@
+import React, { memo } from 'react';
+import { cn } from '@/shared/lib/classNames/classNames';
+import s from './Icon.module.scss';
+
+type IconSize = 's' | 'm' | 'l';
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+    svg: React.VFC<React.SVGProps<SVGSVGElement>>;
+    size?: IconSize;
+    className?: string;
+}
+
+export const Icon = memo((props: IconProps) => {
+    const { svg, size = 'm', className = '', ...otherProps } = props;
+
+    const Svg = svg;
+
+    return (
+        <Svg
+            className={cn(s.icon, {}, [className, s[`icon_size_${size}`]])}
+            {...otherProps}
+        />
+    );
+});

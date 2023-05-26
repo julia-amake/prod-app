ca10265b05024801b4f27e135a9f0aa688372361
Yulia Starchenko <imay@amake.ru>
2023-05-26 14:08:17 +0300

    Added: toggle features to article type tabs

--- src/features/articleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx
+++ src/features/articleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx
@@ -1,6 +1,8 @@
 import React, { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { TabItem, Tabs } from '@/shared/ui/deprecated/Tabs';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { TabItem, Tabs as TabsDeprecated } from '@/shared/ui/deprecated/Tabs';
+import { Tabs } from '@/shared/ui/redesigned/Tabs';
 import { ArticleType } from '../../../../entities/Article/model/consts/consts';
 
 interface ArticleTypeTabsProps {
@@ -49,12 +51,27 @@ export const ArticleTypeTabs = memo((props: ArticleTypeTabsProps) => {
     );
 
     return (
-        <Tabs
-            tabs={tabsList}
-            value={value}
-            onTabClick={onTabClick}
-            disabled={isLoading}
-            className={className}
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={
+                <Tabs
+                    tabs={tabsList}
+                    value={value}
+                    direction="column"
+                    onTabClick={onTabClick}
+                    disabled={isLoading}
+                    className={className}
+                />
+            }
+            off={
+                <TabsDeprecated
+                    tabs={tabsList}
+                    value={value}
+                    onTabClick={onTabClick}
+                    disabled={isLoading}
+                    className={className}
+                />
+            }
         />
     );
 });

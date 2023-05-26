902a15f7c7944a7225ff0a4ec820417dbe8b73e8
Yulia Starchenko <imay@amake.ru>
2023-05-26 13:55:21 +0300

    Created: redesigned card

---
+++ src/shared/ui/redesigned/Card/Card.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import { Text } from '../Text/Text';
+import { Card } from './Card';
+
+export default {
+    title: 'shared/Card',
+    component: Card,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => (
+    <Card {...args}>
+        <Text content="Карточка с текстом" />
+    </Card>
+);
+
+export const Normal = Template.bind({});

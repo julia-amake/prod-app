d631537df9806dd413cbf477f6852c8ca1c681d1
Yulia Starchenko <imay@amake.ru>
2023-05-31 22:01:51 +0300

    Updated: modal

---
+++ src/shared/ui/redesigned/Modal/Modal.tsx
@@ -0,0 +1,103 @@
+import React, { ReactNode } from 'react';
+import { cn, Mode } from '@/shared/lib/classNames/classNames';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { useModal } from '@/shared/lib/hooks/useModal/useModal';
+import {
+    Heading as HeadingDeprecated,
+    HeadingSize,
+} from '../../deprecated/Heading/Heading';
+import { Text as TextDeprecated } from '../../deprecated/Text/Text';
+import { Card } from '../Card';
+import { Heading } from '../Heading';
+import { Overlay } from '../Overlay/Overlay';
+import { Portal } from '../Portal/Portal';
+import { Text } from '../Text';
+import s from './Modal.module.scss';
+
+interface ModalProps {
+    title?: string;
+    subtitle?: string;
+    isOpen?: boolean;
+    className?: string;
+    children?: ReactNode;
+    onClose?: () => void;
+    lazy?: boolean;
+}
+
+export const Modal: React.FC<ModalProps> = (props) => {
+    const {
+        title,
+        subtitle,
+        isOpen,
+        onClose,
+        className = '',
+        children,
+        lazy = false,
+    } = props;
+
+    const { isShown, isMounted, isClosing, close } = useModal({
+        isOpen,
+        onClose,
+        animationDelay: 300,
+    });
+
+    const modalMods: Mode = {
+        [s.opened]: isShown,
+        [s.isClosing]: isClosing,
+    };
+
+    if (lazy && !isMounted) return null;
+    return (
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={
+                <Portal element={document.getElementById('app')}>
+                    <div className={cn(s.modal, modalMods, [className])}>
+                        <Overlay onClick={close} />
+                        <Card className={s.redesigned_content} paddings="l">
+                            {title && (
+                                <Heading
+                                    content={title}
+                                    size="m"
+                                    className={s.title}
+                                    as="h3"
+                                />
+                            )}
+                            {subtitle && (
+                                <Text
+                                    content={subtitle}
+                                    className={s.subtitle}
+                                />
+                            )}
+                            {children}
+                        </Card>
+                    </div>
+                </Portal>
+            }
+            off={
+                <Portal element={document.getElementById('app')}>
+                    <div className={cn(s.modal, modalMods, [className])}>
+                        <Overlay onClick={close} />
+                        <div className={s.content}>
+                            {title && (
+                                <HeadingDeprecated
+                                    content={title}
+                                    size={HeadingSize.S}
+                                    className={s.title}
+                                    as="h3"
+                                />
+                            )}
+                            {subtitle && (
+                                <TextDeprecated
+                                    content={subtitle}
+                                    className={s.subtitle}
+                                />
+                            )}
+                            {children}
+                        </div>
+                    </div>
+                </Portal>
+            }
+        />
+    );
+};
